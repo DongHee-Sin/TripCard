b1e8a5c050fedd40444d7c965f684972fc60ee9d
DongHee-Sin <voslr0601@naver.com>
2022-09-06 20:56:09 +0900

    Initial Commit

---
+++ TripCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TripCard
+//
+//  Created by 신동희 on 2022/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
