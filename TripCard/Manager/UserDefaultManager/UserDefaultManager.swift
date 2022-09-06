1ef0e9960cd12d33574c3bb246fc7560d4a4ffc7
DongHee-Sin <voslr0601@naver.com>
2022-09-06 21:28:49 +0900

    UserDefaults Manager 생성

---
+++ TripCard/Manager/UserDefaultManager/UserDefaultManager.swift
@@ -0,0 +1,45 @@
+//
+//  UserDefaultManager.swift
+//  TripCard
+//
+//  Created by 신동희 on 2022/09/06.
+//
+
+import Foundation
+
+
+@propertyWrapper
+struct UserDefault<T> {
+    let key: String
+    let defaultValue: T
+    
+    var wrappedValue: T {
+        get { UserDefaults.standard.object(forKey: self.key) as? T ?? defaultValue }
+        set { UserDefaults.standard.set(newValue, forKey: self.key) }
+    }
+}
+
+
+class UserDefaultManager {
+    static let shared = UserDefaultManager()
+    
+    private init() {}
+    
+    // MARK: - Propertys
+    @UserDefault(key: "font", defaultValue: "defaultFont")
+    var font: String
+    
+    
+    @UserDefault(key: "isInitialLaunch", defaultValue: false)
+    var isInitialLaunch: Bool
+    
+    
+    
+    
+    // MARK: - Method
+    func resetAllData() {
+        if let bundleID = Bundle.main.bundleIdentifier {
+            UserDefaults.standard.removePersistentDomain(forName: bundleID)
+        }
+    }
+}
