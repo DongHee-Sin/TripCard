f004d9ba943ba59a101f45088d0aaf05a7892ae7
DongHee-Sin <voslr0601@naver.com>
2022-09-07 00:37:07 +0900

    다국어 대응 구조 생성

---
+++ TripCard/Utility/Extension/String+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  String+Extension.swift
+//  TripCard
+//
+//  Created by 신동희 on 2022/09/07.
+//
+
+import Foundation
+
+
+extension String {
+    
+    var localized: String {
+        return NSLocalizedString(self, comment: "")
+    }
+    
+}
