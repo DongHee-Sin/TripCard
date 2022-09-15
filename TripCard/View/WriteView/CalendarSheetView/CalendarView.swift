1d380b0bbd3d9e965411a7b39e240df31e5ee343
DongHee-Sin <voslr0601@naver.com>
2022-09-16 00:18:24 +0900

    FSCalendar UI 설정 (Color, Font 등등..)

--- TripCard/View/WriteView/CalendarSheetView/CalendarView.swift
+++ TripCard/View/WriteView/CalendarSheetView/CalendarView.swift
@@ -13,7 +13,17 @@ final class CalendarView: BaseView {
     
     // MARK: - Propertys
     let calendar = FSCalendar().then {
-//        $0.allowsMultipleSelection = true
+        $0.layer.cornerRadius = 10
+        $0.backgroundColor = .white
+        
+        $0.today = nil
+        $0.appearance.selectionColor = ColorManager.shared.buttonColor
+        $0.appearance.weekdayFont = .customFont(size: .normal)
+        $0.appearance.titleFont = .customFont(size: .small)
+        $0.appearance.headerTitleFont = .customFont(size: .normal)
+        
+        $0.appearance.titleWeekendColor = .red
+        $0.allowsMultipleSelection = true
         $0.swipeToChooseGesture.isEnabled = true
     }
     
@@ -27,7 +37,7 @@ final class CalendarView: BaseView {
     
     override func setConstraint() {
         calendar.snp.makeConstraints { make in
-            make.edges.equalTo(self.safeAreaLayoutGuide).inset(12)
+            make.edges.equalTo(self.safeAreaLayoutGuide).inset(20)
         }
     }
 }
