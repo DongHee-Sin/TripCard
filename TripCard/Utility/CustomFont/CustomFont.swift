e8e7d11c24437b710405d44b9b5b754cf6856cd0
DongHee-Sin <voslr0601@naver.com>
2022-09-28 16:48:44 +0900

    Custom Font 추가 (나눔 스퀘어 라운드)

--- TripCard/Utility/CustomFont/CustomFont.swift
+++ TripCard/Utility/CustomFont/CustomFont.swift
@@ -11,5 +11,6 @@ import Foundation
 enum CustomFont: String, CaseIterable {
     case KyoboHandwriting2020
     case GangwonEduAll = "GangwonEduAll-OTFLight"
+    case NanumSquareRoundOTFL
     case LeferiBaseType = "LeferiBaseType-Regular"
 }
